9c44f720533407a94df1146918247ded485528bb
Datodia <dato.diasamidze.02@gmail.com>
2022-10-18 13:08:58 +0400

    init: initialize project

---
+++ script.js
@@ -0,0 +1,13 @@
+// const title = document.querySelector('#test');
+// const pg = document.querySelector('#pg')
+
+
+// title.addEventListener('click', () => {
+//     // if (pg.style.maxHeight) {
+//     //     pg.style.maxHeight = "0px"
+//     // } else {
+//     //     pg.style.maxHeight = "100px"
+//     // }
+//     pg.classList.toggle('active')
+//     console.log('test')
+// })
