afb2fa3d723536c31ab4ea21734c6860d2813062
Datodia <dato.diasamidze.02@gmail.com>
2022-10-18 13:46:29 +0400

    feat: add toggle

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // const title = document.querySelector('#test');
 // const pg = document.querySelector('#pg')
 
+const item = document.querySelector('.item')
+const answer = document.querySelector('.answer')
+
+item.addEventListener('click', () => {
+    console.log('test')
+    answer.classList.toggle('active')
+})
+
 
 // title.addEventListener('click', () => {
 //     // if (pg.style.maxHeight) {
